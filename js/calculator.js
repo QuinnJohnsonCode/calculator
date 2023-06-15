89acff5bb2915c4ff7c275bce5f646ecba09312e
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-06-14 22:00:18 -0400

    Add basic display updating with number buttons

--- js/calculator.js
+++ js/calculator.js
@@ -2,16 +2,26 @@ import { add, subtract, divide, multiply, operate } from "./functions.js";
 
 function main() {
     // variables to be used with operate()
-    let operator;
-    let num1;
-    let num2;
+    let operator = null;
+    let num1 = null;
+    let num2 = null;
+
+    // Get display
+    const display = document.getElementById("display");
 
     // Gather buttons
     const buttons = getAllButtons();
     const numberButtons = buttons.filter(button => isNumber(button.value));
     const functionButtons = buttons.filter(button => !isNumber(button.value));
     
+    // Setup event listeners
+    numberButtons.forEach(button => button.addEventListener("click", event => {
+        updateDisplay(display, event.target.value);
+    }));
+}
 
+function updateDisplay(display, num) {
+    display.textContent = num;
 }
 
 function getAllButtons() {
