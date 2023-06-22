5acd15488671a9cb08a8c8e042da9c549f204a2b
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-06-22 16:33:52 -0400

    Fix bug with animation in click event

--- js/button-ripple.js
+++ js/button-ripple.js
@@ -11,6 +11,9 @@ export function createRipple(e)
     var d = Math.max(this.clientWidth, this.clientHeight);
     circle.style.width = circle.style.height = d + 'px';
 
+    // Fixes bug with animation interfering in click events
+    circle.style.pointerEvents = 'none';
+
     circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
     circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px';
 
